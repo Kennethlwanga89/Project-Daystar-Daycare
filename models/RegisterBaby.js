8b4b8ede60a9b09337b9d9a2b4ff2cbb900fa8cd
Kenneth Lwanga <kennethlwanga@live.com>
2024-05-23 22:28:18 +0300

    Add model schema for register a baby

---
+++ models/RegisterBaby.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const registerBabySchema = new mongoose.Schema ({
+    firstName: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    age: {
+        type: Number,
+        trim: true,
+        required: true
+    },
+    dob: {
+        type: Date,
+        trim: true
+    },
+    gender: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    fatherName: {
+        type: String,
+        required: true
+    },
+    motherName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
